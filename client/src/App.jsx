ca4eefc3eda28772d912b0d3a72ab2dde3e85e54
ShainiAnil <shainij7@gmail.com>
2024-04-26 17:04:10 +0100

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
   
 
   return (
-    <>
-     
-    </>
+    <div className="bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold text-gray-800">Hello, Tailwind CSS!</h1>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">Button111</button>
+    </div>
   )
 }
 
