5736bd7fef6e0781328ef97b139e4bd6fb3626c5
ShainiAnil <shainij7@gmail.com>
2024-05-01 23:07:27 +0100

    error fixed on main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { UserProvider } from "./context/UserContext.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
