473e76c82fd418635d38d3d03a0f2ab625dcb6be
ShainiAnil <shainij7@gmail.com>
2024-05-02 01:33:52 +0100

    fixed bugs

--- client/src/components/GoogleCalendar/GoogleCalendar.jsx
+++ client/src/components/GoogleCalendar/GoogleCalendar.jsx
@@ -1,37 +1,36 @@
 import React from "react";
 import { useGoogleLogin } from "@react-oauth/google";
 import { extractTokens } from "../../utils/api";
-import "../BookEvent/BookEvent.css"
+import "../BookEvent/BookEvent.css";
 
-const GoogleCalendar = ({eventId}) => {
-    const login = useGoogleLogin({
-        onSuccess: (googleResponse) => {
-          const { code } = googleResponse;
-          console.log("login successful",  code );
-    
-          extractTokens(eventId, code).then((serverResponse) => {
-            if (serverResponse) {
-              console.log("event added to calendar", serverResponse);
-            }
-            alert("Event added to Google calendar");
-          });
-        },
-        flow: "auth-code",
-        scope: "https://www.googleapis.com/auth/calendar",
-        onError: (err) => {
-          console.log("Login Failed", err);
-          alert("Google login failed");
-        },
+const GoogleCalendar = ({ eventId }) => {
+  const login = useGoogleLogin({
+    onSuccess: (googleResponse) => {
+      const { code } = googleResponse;
+      console.log("login successful", { code });
+
+      extractTokens(eventId, { code }).then((serverResponse) => {
+        if (serverResponse) {
+          console.log("event added to calendar", serverResponse);
+        }
+        alert("Event added to Google calendar");
       });
-    
-      return (
-        <>
-          <div className="book-container"> 
-            <button  onClick={() => login()}>Add to Calendar</button>
-          </div>
-        </>
-      );
-    };
-    
+    },
+    flow: "auth-code",
+    scope: "https://www.googleapis.com/auth/calendar",
+    onError: (err) => {
+      console.log("Login Failed", err);
+      alert("Google login failed");
+    },
+  });
 
-export default GoogleCalendar
+  return (
+    <>
+      <div className="book-container">
+        <button onClick={() => login()}>Add to Calendar</button>
+      </div>
+    </>
+  );
+};
+
+export default GoogleCalendar;
