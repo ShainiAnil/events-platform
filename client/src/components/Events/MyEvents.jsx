10161cd2f68b23f4302a9902d81d9b564a513880
ShainiAnil <shainij7@gmail.com>
2024-05-02 15:15:41 +0100

    updated events

--- client/src/components/Events/MyEvents.jsx
+++ client/src/components/Events/MyEvents.jsx
@@ -36,7 +36,7 @@ const MyEvents = () => {
   return (
     <>
       <div className="main">
-        <h1>Your Ultimate Guide to What's Happening Near You</h1>
+        <h1>Your Bookings</h1>
         <ul className="cards">
           {events.map(
             ({
@@ -58,7 +58,8 @@ const MyEvents = () => {
                     </div>
                     <div className="card_content">
                       <h2 className="card_title">{title}</h2>
-                      <p className="card_text">{description}</p>
+                      <p className="card_text">Number of attendees:<span>
+                        </span></p>
                       <Link to={`/events/${_id}`}>
                         <button className="btn card_btn">Read More</button>
                       </Link>
