38736a310ca30cd7f038164f994362faeaf5f54a
ShainiAnil <shainij7@gmail.com>
2024-05-02 16:35:33 +0100

    addes styles to eventCard

--- client/src/components/Events/MyEvents.jsx
+++ client/src/components/Events/MyEvents.jsx
@@ -6,18 +6,18 @@ import UserContext from "../../context/UserContext";
 import { convertTimestamp } from "../../utils/util";
 const MyEvents = () => {
   const [events, setEvents] = useState([]);
-  const [myEventsCount, setMyEventsCount]= useState(0)
+  const [myEventsCount, setMyEventsCount] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
   const { user } = useContext(UserContext);
   const userId = user._id;
-  
+
   useEffect(() => {
     setIsLoading(true);
     myEvents({ userId }).then((data) => {
       setEvents(data.myEventsList);
       setMyEventsCount(data.myEventsCount);
     });
-    
+
     setIsLoading(false);
   }, []);
 
@@ -27,29 +27,19 @@ const MyEvents = () => {
   //       <p>Loading...</p>
   //     </div>
   //   );
-    if(events.length === 0 && myEventsCount === 0)
-    return(
+  if (events.length === 0 && myEventsCount === 0)
+    return (
       <div className="isLoading">
         <p>No Bookings</p>
       </div>
-  )
+    );
   return (
     <>
       <div className="main">
         <h1>You have {myEventsCount} Bookings</h1>
         <ul className="cards">
           {events.map(
-            ({
-              _id,
-              title,
-              startDate,
-              endDate,
-              description,
-              location,
-              price,
-              image,
-              attendees,
-            }) => {
+            ({ _id, title, startDate, location, image, attendees }) => {
               return (
                 <li className="cards_item" key={_id}>
                   <div className="card">
@@ -58,12 +48,15 @@ const MyEvents = () => {
                     </div>
                     <div className="card_content">
                       <h2 className="card_title">{title}</h2>
-                      <p className="card_text">🧑 Attendees:<span>{attendees.length}
-                        </span></p>
-                        <p className="card_text">📍Location:<span>{location}
-                        </span></p>
-                        <p className="card_text">⌚Time:<span>{convertTimestamp(startDate).time}
-                        </span></p>
+                      <p className="card_text">
+                        🧑 Attendees:<span className="card-info">{attendees.length}</span>
+                      </p>
+                      <p className="card_text">
+                        📍Location:<span className="card-info">{location}</span>
+                      </p>
+                      <p className="card_text">
+                        ⌚Time:<span className="card-info">{convertTimestamp(startDate).time}</span>
+                      </p>
                       <Link to={`/events/${_id}`}>
                         <button className="btn card_btn">Read More</button>
                       </Link>
@@ -75,7 +68,6 @@ const MyEvents = () => {
           )}
         </ul>
       </div>
-
     </>
   );
 };
